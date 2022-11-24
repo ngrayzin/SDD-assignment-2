a58a1677a9f0ed54e5714ac91752c6e5c9cec803
Ray Zin Ng <s10222457@connect.np.edu.sg>
2022-11-24 18:50:50 +0800

    Initial commit

---
+++ lib/firebase_options.dart
@@ -0,0 +1,43 @@
+// TODO Implement this library.
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'firebase_options.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: '',
+    appId: '1:797884859033:android:352af6d6c140f45f71ae08',
+    messagingSenderId: '797884859033',
+    projectId: 'sdd-assignment-2',
+  );
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: '',
+    appId: '1:797884859033:ios:600da01a783a495371ae08',
+    messagingSenderId: '797884859033',
+    projectId: 'sdd-assignment-2',
+    iosBundleId: 'org.sddassignment.firebase',
+  );
+}
