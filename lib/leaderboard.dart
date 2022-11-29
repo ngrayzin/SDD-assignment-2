5366521bb08c1b7d976d85b2c263248a445327b3
Jin Daat <s10222867@connect.np.edu.sg>
2022-11-29 11:58:24 +0800

    Clean up leaderboard front end

--- lib/leaderboard.dart
+++ lib/leaderboard.dart
@@ -12,27 +12,74 @@ class LeaderBoard extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('N.A.C\'s\''),
+        title: const Text('N.A.C\'s'),
       ),
       body: Center(
           child: Column(
         children: [
           Container(
-            margin: const EdgeInsets.fromLTRB(10, 37.74, 10, 3),
+            margin: const EdgeInsets.fromLTRB(20, 20, 20, 10),
             padding: const EdgeInsets.all(5.0),
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            child: const Text(
+              "LeaderBoard",
+              textAlign: TextAlign.center,
+            ),
+          ), //Container for leaderboard title
+          Container(
+            margin: const EdgeInsets.fromLTRB(10.0, 10, 10, 2),
+            padding: const EdgeInsets.all(5.0),
+            decoration: const BoxDecoration(
               borderRadius: BorderRadius.all(Radius.circular(15)),
             ),
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text("Title Player Position"),
-                  const Text("Title Player Name"),
-                  const Text("Title Player Score"),
+                  const Text("Player Position"),
+                  const Text("Player Name"),
+                  const Text("Player Score"),
+                ]),
+          ), //Container for Leaderboard Title
+          Container(
+            margin: const EdgeInsets.fromLTRB(5.0, 2, 5, 2),
+            padding: const EdgeInsets.all(5.0),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.red,
+                width: 5.0,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Player Position"),
+                  const Text("Player Name"),
+                  const Text("Player Score"),
+                ]),
+          ), //Container for player leaderboard attributes
+          Container(
+            margin: const EdgeInsets.fromLTRB(5.0, 2, 5, 2),
+            padding: const EdgeInsets.all(5.0),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.red,
+                width: 5.0,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Player Position"),
+                  const Text("Player Name"),
+                  const Text("Player Score"),
                 ]),
           ),
           Container(
-            margin: const EdgeInsets.all(5.0),
+            margin: const EdgeInsets.fromLTRB(5.0, 2, 5, 2),
             padding: const EdgeInsets.all(5.0),
             decoration: BoxDecoration(
               border: Border.all(
