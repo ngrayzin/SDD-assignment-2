795b086d5d5a15fa290b43ab9a557a9068d3b131
Ryanyzh <S10219317@connect.np.edu.sg>
2022-12-05 11:54:29 +0800

    add property to player class

--- lib/Player.dart
+++ lib/Player.dart
@@ -2,6 +2,8 @@ class Player {
   final String name;
   final List<String> map;
   int turn = 0;
+  int point = 0;
+  int coin = 0;
 
   Player(this.name, this.map);
   void addItemToMap(int i, String n) {
