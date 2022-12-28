45aa37724406e5ab2648e4fcc088a1fccf8ec330
Ray Zin Ng <rayzin147@gmail.com>
2022-12-28 23:06:25 +0800

    done load game

--- lib/MainMenu.dart
+++ lib/MainMenu.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flame/flame.dart'; // ADDED FLAME INTO DART FILE
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart'; // ADDED GOOGLE FONTS
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sdd_assignment_2/About.dart';
@@ -12,14 +13,15 @@ import 'package:sdd_assignment_2/GameLevel.dart';
 import 'package:sdd_assignment_2/GamePage.dart';
 import 'package:sdd_assignment_2/LoadGame.dart';
 import 'package:sdd_assignment_2/PopUpMessage.dart';
+import 'Player.dart';
 import 'colours.dart' as colours;
 import 'Firebase_options.dart';
 import "Leaderboard.dart";
 
-final GoogleSignIn _googleSignIn = GoogleSignIn(
+/*final GoogleSignIn _googleSignIn = GoogleSignIn(
   signInOption: SignInOption.games,
   scopes: ['email'],
-);
+);*/
 
 class MainMenu extends StatefulWidget {
   const MainMenu({super.key});
@@ -30,7 +32,7 @@ class MainMenu extends StatefulWidget {
 }
 
 class _MainMenuState extends State<MainMenu> {
-  GoogleSignInAccount? _currentUser;
+/*  GoogleSignInAccount? _currentUser;
   @override
   void initState() {
     super.initState();
@@ -43,7 +45,7 @@ class _MainMenuState extends State<MainMenu> {
       }
     });
     _googleSignIn.signInSilently();
-  }
+  }*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,10 +111,40 @@ class _MainMenuState extends State<MainMenu> {
                 child: SizedBox(
                   width: MediaQuery.of(context).size.width * 0.75,
                   child: TextButton(
-                    onPressed: () => Navigator.push(context,
+                    onPressed: () async {
+                      final ref = FirebaseDatabase.instance.ref();
+                      var currentUser = FirebaseAuth.instance.currentUser;
+                      await ref.child('players/${currentUser?.uid}/saveGame').get()
+                          .then((snapshot){
+                            if (snapshot.exists) {
+                              var list = [];
+                              for (var item in snapshot.children) {
+                                print(item.value);
+                                list.add(item.value);
+                              }
+                              Player player = Player(currentUser?.displayName, [], list[4], list[0]);
+                              player.stringMap = list[2];
+                              player.deserializeMap();
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                    return LoadGame(player: player, coin: list[0], level: list[1], point: list[3],);
+                                  }));
+                            }
+                            else {
+                              Fluttertoast.showToast(
+                                  msg: "This is a Toast message",
+                                  toastLength: Toast.LENGTH_SHORT,
+                                  timeInSecForIosWeb: 1,
+                                  textColor: Colors.white,
+                                  fontSize: 16.0
+                              );
+                            }
+                        });
+                      /*Navigator.push(context,
                         MaterialPageRoute(builder: (context) {
                       return const LoadGame();
-                    })),
+                    }));*/
+                    },
                     style: ButtonStyle(
                         padding: MaterialStateProperty.all<EdgeInsets>(
                             EdgeInsets.fromLTRB(0, 16.0, 0, 16.0)),
@@ -199,4 +231,5 @@ class _MainMenuState extends State<MainMenu> {
           ),
         ));
   }
+
 }
